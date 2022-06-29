2526a7726d69865c9daec88db6d9a09153575326
Vitória Meinerz <calvi.vitoria@gmail.com>
2022-06-29 18:14:40 -0300

    começa req 6

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -3,28 +3,76 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 class Game extends React.Component {
+  state = {
+    responseAPI: '',
+  }
+
+  componentDidMount() {
+    const ONE_MINUTE = 60000;
+    this.triviaApiRequest();
+    setTimeout(this.tokenHasExpired, ONE_MINUTE);
+  }
+
+  triviaApiRequest = async () => {
+    const token = localStorage.getItem('token');
+    const apiData = await fetch(`https://opentdb.com/api.php?amount=5&token=${token}`);
+    const response = await apiData.json();
+    this.setState({ responseAPI: response });
+    console.log(response);
+    return response;
+  };
+
+  // caso token seja inválido
+  tokenHasExpired = () => {
+    const { responseAPI } = this.state;
+    const { history } = this.props;
+    const logOut = {
+      response_code: 3,
+      results: [],
+    };
+    if (responseAPI === logOut) {
+      localStorage.removeItem('token');
+      localStorage.removeItem('name');
+      history.push('/');
+    }
+  }
+
+  // // handle click respostas
+  // handleClick = () => {
+  //   const { responseAPI } = this.state;
+  //   responseAPI.map((array, index) => {
+
+  //   });
+  // }
+
   render() {
-    const { gravatarEmail, name } = this.props;
+    const { gravatarEmail } = this.props;
+    const { responseAPI } = this.state;
     return (
-      <header>
-        <img
-          data-testid="header-profile-picture"
-          alt="seu gravatar"
-          src={ `https://www.gravatar.com/avatar/${gravatarEmail}` }
-        />
-        <p
-          data-testid="header-player-name"
-        >
-          {name}
+      <>
+        <header>
+          <img
+            data-testid="header-profile-picture"
+            alt="seu gravatar"
+            src={ `https://www.gravatar.com/avatar/${gravatarEmail}` }
+          />
+          <p
+            data-testid="header-player-name"
+          >
+            { localStorage.getItem('name') }
 
-        </p>
-        <p
-          data-testid="header-score"
-        >
-          Placar: 0
+          </p>
+          <p
+            data-testid="header-score"
+          >
+            Placar: 0
 
-        </p>
-      </header>
+          </p>
+        </header>
+        <section>
+          { responseAPI.results.map((array, index) => (<h2 data-testid="question-text" key={ index }>{ array.category }</h2>))}
+        </section>
+      </>
     );
   }
 }
@@ -36,10 +84,10 @@ const mapStateToProps = (state) => ({
 
 Game.propTypes = {
   gravatarEmail: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
-  // history: PropTypes.shape({
-  //   push: PropTypes.func.isRequired,
-  // }).isRequired,
+  // name: PropTypes.string.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
 };
 
 export default connect(mapStateToProps)(Game);
