475da83cb42e56e9ca604a7c058e1fa442bad806
Vitória Meinerz <calvi.vitoria@gmail.com>
2022-06-29 14:53:25 -0300

    adiciona mais um teste: usuário consegue escrever nos inputs

--- src/tests/helpers/Login.test.js
+++ src/tests/helpers/Login.test.js
@@ -7,12 +7,13 @@ import { createMemoryHistory } from 'history';
 import { Provider } from 'react-redux';
 import rootReducer from "../../reducer";
 import { legacy_createStore as createStore } from 'redux';
+import userEvent from "@testing-library/user-event";
 
 
 describe(
     'Testa o componente Login', () => {
       test(
-        'Ao renderizar a tela os Inputs são renderizados', () => {
+        'Ao renderizar a tela os Inputs são mostrados', () => {
           const history = createMemoryHistory();
           const store = createStore(rootReducer);
             render(
@@ -31,7 +32,7 @@ describe(
         }
       );
         test(
-            'Ao renderizar a tela os Botões são renderizados', () => {
+            'Ao renderizar a tela os Botões são mostrados', () => {
               const history = createMemoryHistory();
               const store = createStore(rootReducer);
                 render(
@@ -53,6 +54,29 @@ describe(
                 expect(settings).toBeInTheDocument();
             }
         );
+
+        test(
+          'O usuário consegue digitar nos inputs', () => {
+            const history = createMemoryHistory();
+            const store = createStore(rootReducer);
+              render(
+                <Provider store= { store }>
+                  <Router history={ history }>
+                    <App />
+                  </Router>
+                </Provider>,
+              );
+              
+              const name = screen.getByPlaceholderText('name');
+              const email = screen.getByPlaceholderText('email');
+              userEvent.type(name, 'sandy');
+              expect(name).toHaveValue('sandy');
+
+              userEvent.type(email, 'sandy@betrybe.com');
+              expect(email).toHaveValue('sandy@betrybe.com');
+              
+          }
+      );
 
      
     }
