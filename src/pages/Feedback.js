ddead9d6f24a0ba132f4767500741c6cb950b38c
Vitória Meinerz <calvi.vitoria@gmail.com>
2022-06-30 14:11:40 -0300

    finaliza req 12

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -6,27 +6,45 @@ const MINIMUM_SCORE = 3;
 
 class Feedback extends React.Component {
   render() {
-    const { score } = this.props;
+    const { score, gravatar } = this.props;
     return (
-      <section>
-        {/* Inserir header aqui */}
+      <header>
+        <img
+          data-testid="header-profile-picture"
+          alt="seu gravatar"
+          src={ `https://www.gravatar.com/avatar/${gravatar}` }
+        />
+        <p
+          data-testid="header-player-name"
+        >
+          {localStorage.getItem('name')}
+
+        </p>
+        <p
+          data-testid="header-score"
+        >
+          {score}
+        </p>
         {
           score >= MINIMUM_SCORE ? (
             <p data-testid="feedback-text">Well Done!</p>
           ) : <p data-testid="feedback-text">Could be better...</p>
         }
 
-      </section>
+      </header>
     );
   }
 }
 
 const mapStateToProps = (state) => ({
   score: state.player.score,
+  name: state.player.name,
+  gravatar: state.player.gravatarEmail,
 });
 
 Feedback.propTypes = {
   score: PropTypes.number.isRequired,
+  gravatar: PropTypes.string.isRequired,
 };
 
 export default connect(mapStateToProps, null)(Feedback);
